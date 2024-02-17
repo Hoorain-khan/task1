e6988156cbdb067ff2df75922b4febbcb037cd28
Hoorain Fareed <hoorainfareed82@gmail.com>
2024-02-18 03:15:23 +0500

    message

---
+++ first.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+var message = "Hello world";
+console.log(message);
+var userInput = prompt('Please enter your name.');
+console.log(userInput);
---
+++ first.ts
@@ -0,0 +1,5 @@
+
+let message : string = "Hello world";
+console.log(message);
+var userInput = prompt('Please enter your name.');
+console.log(userInput);
