124e2412ef0b0f2cef45b57cef2299daedf2edeb
Hoorain Fareed <hoorainfareed82@gmail.com>
2024-02-18 03:18:33 +0500

    message

--- first.js
+++ first.js
@@ -3,3 +3,6 @@ var message = "Hello world";
 console.log(message);
 var userInput = prompt('Please enter your name.');
 console.log(userInput);
+
+var hoorain = prompt('Please enter your name.');
+console.log(hoorain);
